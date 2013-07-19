84de4a9a1436308bf38a841f61afb9e1d54d7395
Kunal Varma <kunalvarma05@gmail.com>
2013-07-20 05:24:16 +0530

    Div Adjustments

--- footer.php
+++ footer.php
@@ -1,13 +1,14 @@
-		<footer class="footer">
-				<div class="inner clearfix">
-					<div class="left copyright">
-						&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>.
-					</div>
-					<div class="right meta">
-						Made with <a href="http://anchorcms.com" class="anchor">Anchor</a> by <a href="http://kunalvarma.in" class="creator">Kunal Varma</a>
-					</div>
-				</div>
-			</footer>	        
-        </div>
-    </body>
+<footer class="footer">
+	<div class="inner clearfix">
+		<div class="left copyright">
+			&copy; <?php echo date('Y'); ?>
+			<?php echo site_name(); ?>.
+		</div>
+		<div class="right meta">
+			Made for <a href="http://anchorcms.com" class="anchor">Anchor</a> by <a href="http://kunalvarma.in" class="creator">Kunal Varma</a>
+		</div>
+	</div>
+</footer>
+</div>
+</body>
 </html>
