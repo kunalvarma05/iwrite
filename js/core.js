aeec79149b830d3582cfc3d5691196511be77597
Kunal Varma <kunalvarma05@gmail.com>
2013-07-22 16:48:13 +0530

    List Wrap item contents in <p> tag

--- js/core.js
+++ js/core.js
@@ -7,4 +7,6 @@ jQuery('.search-toggle').click(function() {
 jQuery('.search-bar').slideToggle(200);
 jQuery('.search-box').focus();
 });
+jQuery('.single-post .post-content ol').not('.items').children('li').wrapInner('<p>');
+jQuery('.single-post .post-content ul').not('.items').children('li').wrapInner('<p>');
 });
