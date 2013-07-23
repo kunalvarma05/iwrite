b664621725cc2d99dc9140299a0a63818c51212f
Kunal Varma <kunalvarma05@gmail.com>
2013-07-23 18:28:22 +0530

    Adjustments

--- page.php
+++ page.php
@@ -7,7 +7,7 @@
 							<h2 class="post-title" itemprop="headline"><a href="<?php echo page_url(); ?>" title="<?php echo page_title(); ?>" rel="bookmark"><?php echo page_title(); ?></a></h2>							
 						</header>
 						<div class="post-content" itemprop="text">
-							<?php echo get_snap('page', 'profile-pic'); ?>							
+							<?php echo get_snap('page', 'featured-image'); ?>							
 							<p>
 								<?php
 								echo page_content();
